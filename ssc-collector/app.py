b178f9fd04af9eedd6d1707f5085bd94c980843f
Themar Yasser <themar11.yassir@gmail.com>
2026-03-15 04:19:14 +0300

    app.py

--- ssc-collector/app.py
+++ ssc-collector/app.py
@@ -115,13 +115,7 @@ def log_event(event):
  def validate_session():
 
     provided_token = request.headers.get("Authorization")
-
-    ip = request.headers.get("X-Forwarded-For")
-    if ip:
-        ip = ip.split(",")[0].strip()
-    else:
-        ip = request.remote_addr
-
+    ip = request.headers.get("X-Forwarded-For", request.remote_addr)
     ua = request.headers.get("User-Agent")
 
     if not provided_token:
